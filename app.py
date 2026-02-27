2bbfe7f9be10cc99cfd4ad7a958a7dbec37f8c99
Shantanu0015 <shantanu15feb0000@gmail.com>
2026-02-27 23:38:20 +0530

    Added automatic DB table creation

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("quiz.db")
+    c = conn.cursor()
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role TEXT DEFAULT 'user'
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+init_db()
 # =========================
 # IMPORTS
 # =========================
